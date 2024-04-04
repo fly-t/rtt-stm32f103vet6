eb6d458450c2c51b895c8075391df02302bef4d2
fly-t <fly-t@163.com>
2024-04-04 11:56:42 +0800

    times

--- applications/main.c
+++ applications/main.c
@@ -13,10 +13,63 @@
 #include <board.h>
 
 /* defined the LED0 pin: PB1 */
-#define LED0_PIN    GET_PIN(B, 5)
+#define LED0_PIN    GET_PIN(B, 1)
 
+
+
+/* 线程 1 入口 */
+void thread1_entry(void* parameter)
+{
+    int i=0;
+
+    while (1)
+    {
+        i+=1;
+        rt_kprintf("thread-1:%d\n", i);
+
+        /* 延时 100ms */
+//        rt_thread_mdelay(1000);
+        if (i>200){
+            return;
+        }
+    }
+}
+
+/* 线程 2 入口 */
+void thread2_entry(void* parameter)
+{
+    int i=0;
+
+    while (1)
+    {
+        i+=1;
+        rt_kprintf("thread--2:%d\n", i);
+
+        /* 延时 100ms */
+//        rt_thread_mdelay(1000);
+        if (i>200){
+            return;
+        }
+    }
+}
 int main(void)
 {
+    rt_thread_t thread2_ptr;
+    rt_thread_t thread1_ptr;
+    thread1_ptr = rt_thread_create("thread1",
+                                   thread1_entry, RT_NULL,
+                                   512, 30, 20);
+
+    thread2_ptr = rt_thread_create("thread2",
+                                   thread2_entry, RT_NULL,
+                                   512, 30, 10);
+
+    /* 启动线程 */
+    if (thread1_ptr != RT_NULL) rt_thread_startup(thread1_ptr);
+    if (thread2_ptr != RT_NULL) rt_thread_startup(thread2_ptr);
+
+
+
     int count = 1;
     /* set LED0 pin mode to output */
     rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
@@ -28,6 +81,8 @@ int main(void)
         rt_pin_write(LED0_PIN, PIN_LOW);
         rt_thread_mdelay(500);
     }
+
+
 
     return RT_EOK;
 }
