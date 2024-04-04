9c34b376d7df27c1b9110607c4beacd22f2b6ff7
fly-t <fly-t@163.com>
2024-04-04 20:05:12 +0800

    mutex

--- applications/main.c
+++ applications/main.c
@@ -15,7 +15,7 @@
 /* defined the LED0 pin: PB1 */
 #define LED0_PIN    GET_PIN(B, 1)
 
-static rt_sem_t sem_demo = RT_NULL;
+static rt_mutex_t dynamic_mutex = RT_NULL;
 
 int i=0;
 /* 线程 1 入口 */
@@ -25,12 +25,13 @@ void thread1_entry(void* parameter)
 
     while (1)
     {
+        rt_mutex_take(dynamic_mutex,RT_WAITING_FOREVER);
         i+=1;
         rt_kprintf("thread-1:%d\n", i);
 
         /* 延时 100ms */
         rt_thread_mdelay(1000);
-        rt_sem_release(sem_demo);
+        rt_mutex_release(dynamic_mutex);
     }
 }
 
@@ -41,11 +42,13 @@ void thread2_entry(void* parameter)
 
     while (1)
     {
-        if(rt_sem_take(sem_demo,RT_WAITING_FOREVER)==RT_EOK){
+        /* mutex 如果获取失败那就直接将线程挂起 waitting forever 不需要判断*/
+        if(rt_mutex_take(dynamic_mutex,RT_WAITING_FOREVER)==RT_EOK){
             i+=1;
             rt_kprintf("thread--2:%d\n", i);
             /* 延时 100ms */
-//            rt_thread_mdelay(1000);
+            rt_thread_mdelay(5000);
+            rt_mutex_release(dynamic_mutex);
         }
 
     }
@@ -53,14 +56,14 @@ void thread2_entry(void* parameter)
 int main(void)
 {
     /* value 0 means only one source */
-    sem_demo = rt_sem_create("dsem", 0, RT_IPC_FLAG_PRIO);
+    dynamic_mutex = rt_mutex_create("mute", RT_IPC_FLAG_PRIO);
 
 
     rt_thread_t thread2_ptr;
     rt_thread_t thread1_ptr;
     thread1_ptr = rt_thread_create("thread1",
                                    thread1_entry, RT_NULL,
-                                   512, 30, 20);
+                                   512, 30, 10);
 
     thread2_ptr = rt_thread_create("thread2",
                                    thread2_entry, RT_NULL,
